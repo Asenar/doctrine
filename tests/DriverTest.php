796b1f5a5a63b510fb31361996fd2e832aac49dc
Robert-Jan de Dreu <robertjandreu@gmail.com>
2015-07-29 15:12:04 +0200

    Added more tests for the drivers

--- tests/DriverTest.php
+++ tests/DriverTest.php
@@ -26,6 +26,58 @@ class DriverTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
+    public function shouldThrowExceptionWhenNoMappingPath()
+    {
+        $this->setExpectedException('Kohana\Doctrine\Exception\IncorrectConfigurationException');
+
+        $this->configuration->shouldReceive('get')
+            ->with('mapping')
+            ->andReturn([
+                'type' => 'annotation',
+                'path' => []
+            ]);
+
+        $this->configuration->shouldReceive('get')
+            ->with('proxy')
+            ->andReturn(['path' => 'path/to/proxy/files']);
+
+        $this->configuration->shouldReceive('get')
+            ->with('onProduction')
+            ->andReturn(true);
+
+        $this->driver->configureDriver();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowExceptionWhenNoProxyPath()
+    {
+        $this->setExpectedException('Kohana\Doctrine\Exception\IncorrectConfigurationException');
+
+        $this->configuration->shouldReceive('get')
+            ->with('mapping')
+            ->andReturn([
+                'type' => 'annotation',
+                'path' => [
+                    'path/to/mapping/files'
+                ]
+            ]);
+
+        $this->configuration->shouldReceive('get')
+            ->with('proxy')
+            ->andReturn(['path' => '']);
+
+        $this->configuration->shouldReceive('get')
+            ->with('onProduction')
+            ->andReturn(true);
+
+        $this->driver->configureDriver();
+    }
+
+    /**
+     * @test
+     */
     public function shouldConfigureAnnotationDriver()
     {
         $this->configuration->shouldReceive('get')
